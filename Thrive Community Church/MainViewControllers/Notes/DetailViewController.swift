67b4d6417771333481d92feb2726be7566f96c56
Wyatt Baggett <wyattbaggett@yahoo.com>
2017-12-25 21:58:26 -0500

    Found the crash issue from #59... begin testing

--- Thrive Community Church/MainViewControllers/Notes/DetailViewController.swift
+++ Thrive Community Church/MainViewControllers/Notes/DetailViewController.swift
@@ -9,14 +9,10 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
-    @IBOutlet weak var detailDescriptionLabel: UITextView!
+    @IBOutlet var detailDescriptionLabel: UITextView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        /*      TODO: Call Parent View controller in order to load the view and also get
-         the new Notes Loaded. Once they have loaded, segue back to this view.
-         */
         
         detailViewController = self
         detailViewController?.becomeFirstResponder()
@@ -25,8 +21,9 @@ class DetailViewController: UIViewController {
         // Called at application init for notes tab - YES
         
         // INIT NOTE #5 - No
+		configureView()
         saveAndUpdate()
-        self.configureView()
+		
         
         // segue
         //INIT NOTE #8 - I assume that this is where the code stops.
@@ -38,7 +35,6 @@ class DetailViewController: UIViewController {
             // Update the view.
             saveAndUpdate()
             self.configureView()
-            
         }
     }
     
@@ -63,11 +59,11 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func share(_ sender: AnyObject) {
-        let textToShare = detailDescriptionLabel.text!
+		let textToShare = detailDescriptionLabel?.text
         
         let objectsToShare = [textToShare]
         let activityVC = UIActivityViewController(activityItems:
-                                            objectsToShare ,
+											objectsToShare,
                                             applicationActivities: nil)
         
         activityVC.popoverPresentationController?.sourceView = (sender) as? UIView
@@ -80,29 +76,27 @@ class DetailViewController: UIViewController {
     }
     
     // called when hitting back on the editing screen -- after segue back to Table View
+	// INIT NOTE #1 - Starts here
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        
-        /*
-         BINGO! - this is called when there is no note in the TableView
-         this is also not called at any time before the user hits back after typing
-         their first message
-         
-         Look here to make changes? -- more testing might be neeeded before we can
-         make that assertion
-        */
+		
         if objects.count == 0 {
             return
         }
-        // INIT NOTE #1 - Still nothing happening
+		
         
         //updates the text for the preview of the note on the Table View
-        objects[currentIndex] = detailDescriptionLabel.text
-        if detailDescriptionLabel.text == "" {
-            objects[currentIndex] = BLANK_NOTE
-        }
-        saveAndUpdate()
-        // Called wheb the view is returning from the editing view
+		
+		// Guard the label to prevent nil from being assigned as the index's label
+		guard let labelIndex = detailDescriptionLabel?.text else { return }
+		
+		objects[currentIndex] = labelIndex
+		if labelIndex == "" {
+			objects[currentIndex] = BLANK_NOTE
+		}
+		
+		saveAndUpdate()
+        // returning from the editing view
     }
     
     // permiate changes to the master view
--- Thrive Community Church/MainViewControllers/Notes/MasterViewController.swift
+++ Thrive Community Church/MainViewControllers/Notes/MasterViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  MasterViewController.swift
 //  notes
 //
@@ -17,163 +17,163 @@ let kNotes:String = "notes"
 let BLANK_NOTE:String = "New Note"
 
 class MasterViewController: UITableViewController {
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        masterView = self
-        // Called when the user Taps "Notes" icon -- buttons are all added before the segue
-        load()
-        self.navigationItem.leftBarButtonItem = self.editButtonItem
-        
-        let addButton = UIBarButtonItem(barButtonSystemItem: .add,
-                                        target: self,
-                                        action: #selector(insertNewObject(_:)))
-        self.navigationItem.rightBarButtonItem = addButton
-        
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
-        // MARK: Back Button
-        // Gets called when the user is returning from writing a note
-        
-        if objects.count == 0 {
-            insertNewObject(self)
-        }
-        
-        save()
-        /*
-         Adding insert new object here in this method is not working - creates an inifinte loop
-         of creating a new table item - but keeps copying the text from other notes to the new one
-         
-        */
-        super.viewWillAppear(animated)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        
-        // Use something like this to check if there are
-         print("Notes - will appear (Master)")
-        //segue to the table view has been made
-        //interactrion is possible now with the UITableView interface
-        
-        // INIT NOTE #4 - Still nothing happening on the Screen --- Showing TableView
-        super.viewDidAppear(animated)
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    // Adds new object & changes name of the string of Master following the segue back
-    @objc func insertNewObject(_ sender: AnyObject) {
-        save()
-        
-        //adding new
-        // INIT NOTE #2 - Nada
-        if objects.count == 0 || objects[0] != BLANK_NOTE {
-            
-            objects.insert(BLANK_NOTE, at: 0)
-            let indexPath = IndexPath(row: 0, section: 0)
-            self.tableView.insertRows(at: [indexPath], with: .automatic)
-            
-            save()
-        }
-        save()
-        
-        currentIndex = 0
-        self.performSegue(withIdentifier: "showDetail", sender: self)
-    }
-    
-    // MARK: - Segues
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            
-            //INIT NOTE #3 - Nothing Still --- may happen after this though
-            if let indexPath = self.tableView.indexPathForSelectedRow {
-                
-                let object = objects[(indexPath as NSIndexPath).row]
-                currentIndex = (indexPath as NSIndexPath).row
-                
-                detailViewController?.detailItem = object as AnyObject?
-                detailViewController?.navigationItem.leftBarButtonItem =
-                            self.splitViewController?.displayModeButtonItem
-                detailViewController?.navigationItem.leftItemsSupplementBackButton = true
-            }
-        }
-    }
-    
-    // MARK: - Table View
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection
-                                                        section: Int) -> Int {
-        return objects.count
-    }
-    
-    override func tableView(_ tableView: UITableView,
-                        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
-                                             for: indexPath)
-        
-        let object = objects[(indexPath as NSIndexPath).row]
-        cell.textLabel!.text = object
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, canEditRowAt
-                                                indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    
-    override func tableView(_ tableView: UITableView, commit
-                                        editingStyle: UITableViewCellEditingStyle,
-                                        forRowAt indexPath: IndexPath) {
-        
-        if editingStyle == .delete {
-            objects.remove(at: (indexPath as NSIndexPath).row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        }
-        else if editingStyle == .insert {
-            // Create a new instance of the appropriate class,
-            // insert it into the array, and add a new row to the table view.
-        }
-    }
-    
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        
-        // selecting an item to be deleted / has been removed - Called multiple times
-        // for various editing tasks
-        if editing {
-            return
-        }
-        save()
-        
-    }
-    
-    override func tableView(_ tableView: UITableView,
-                                didEndEditingRowAt indexPath: IndexPath?) {
-        save()
-        
-    }
-    
-    func save() {
-        UserDefaults.standard.set(objects, forKey: kNotes)
-        UserDefaults.standard.synchronize()
-    }
-    
-    func load() {
-        if let loadedData = UserDefaults.standard.array(forKey: kNotes) as? [String] {
-            objects = loadedData
-        }
-    }
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		
+		masterView = self
+		// Called when the user Taps "Notes" icon -- buttons are all added before the segue
+		load()
+		self.navigationItem.leftBarButtonItem = self.editButtonItem
+		
+		let addButton = UIBarButtonItem(barButtonSystemItem: .add,
+										target: self,
+										action: #selector(insertNewObject(_:)))
+		self.navigationItem.rightBarButtonItem = addButton
+		
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		self.clearsSelectionOnViewWillAppear = self.splitViewController!.isCollapsed
+		// MARK: Back Button
+		// Gets called when the user is returning from writing a note
+		
+		if objects.count == 0 {
+			insertNewObject(self)
+		}
+		
+		save()
+		/*
+		Adding insert new object here in this method is not working - creates an inifinte loop
+		of creating a new table item - but keeps copying the text from other notes to the new one
+		
+		*/
+		super.viewWillAppear(animated)
+	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		
+		// Use something like this to check if there are
+		print("Notes - will appear (Master)")
+		//segue to the table view has been made
+		//interactrion is possible now with the UITableView interface
+		
+		// INIT NOTE #4 - Still nothing happening on the Screen --- Showing TableView
+		super.viewDidAppear(animated)
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	// Adds new object & changes name of the string of Master following the segue back
+	@objc func insertNewObject(_ sender: AnyObject) {
+		save()
+		
+		//adding new
+		// INIT NOTE #2 - Nada
+		if objects.count == 0 || objects[0] != BLANK_NOTE {
+			
+			objects.insert(BLANK_NOTE, at: 0)
+			let indexPath = IndexPath(row: 0, section: 0)
+			self.tableView.insertRows(at: [indexPath], with: .automatic)
+			
+			save()
+		}
+		save()
+		
+		currentIndex = 0
+		self.performSegue(withIdentifier: "showDetail", sender: self)
+	}
+	
+	// MARK: - Segues
+	
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		if segue.identifier == "showDetail" {
+			
+			//INIT NOTE #3 - Nothing Still --- may happen after this though
+			if let indexPath = self.tableView.indexPathForSelectedRow {
+				
+				let object = objects[(indexPath as NSIndexPath).row]
+				currentIndex = (indexPath as NSIndexPath).row
+				
+				detailViewController?.detailItem = object as AnyObject?
+				detailViewController?.navigationItem.leftBarButtonItem =
+					self.splitViewController?.displayModeButtonItem
+				detailViewController?.navigationItem.leftItemsSupplementBackButton = true
+			}
+		}
+	}
+	
+	// MARK: - Table View
+	
+	override func numberOfSections(in tableView: UITableView) -> Int {
+		return 1
+	}
+	
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection
+		section: Int) -> Int {
+		return objects.count
+	}
+	
+	override func tableView(_ tableView: UITableView,
+							cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
+												 for: indexPath)
+		
+		let object = objects[(indexPath as NSIndexPath).row]
+		cell.textLabel!.text = object
+		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, canEditRowAt
+		indexPath: IndexPath) -> Bool {
+		// Return false if you do not want the specified item to be editable.
+		return true
+	}
+	
+	override func tableView(_ tableView: UITableView, commit
+		editingStyle: UITableViewCellEditingStyle,
+							forRowAt indexPath: IndexPath) {
+		
+		if editingStyle == .delete {
+			objects.remove(at: (indexPath as NSIndexPath).row)
+			tableView.deleteRows(at: [indexPath], with: .fade)
+		}
+		else if editingStyle == .insert {
+			// Create a new instance of the appropriate class,
+			// insert it into the array, and add a new row to the table view.
+		}
+	}
+	
+	override func setEditing(_ editing: Bool, animated: Bool) {
+		super.setEditing(editing, animated: animated)
+		
+		// selecting an item to be deleted / has been removed - Called multiple times
+		// for various editing tasks
+		if editing {
+			return
+		}
+		save()
+		
+	}
+	
+	override func tableView(_ tableView: UITableView,
+							didEndEditingRowAt indexPath: IndexPath?) {
+		save()
+		
+	}
+	
+	func save() {
+		UserDefaults.standard.set(objects, forKey: kNotes)
+		UserDefaults.standard.synchronize()
+	}
+	
+	func load() {
+		if let loadedData = UserDefaults.standard.array(forKey: kNotes) as? [String] {
+			objects = loadedData
+		}
+	}
 }
